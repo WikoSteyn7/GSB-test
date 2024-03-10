48181e415e5a0a21381866684ca74bfad9827175
George Earl <geearl@microsoft.com>
2024-03-10 17:21:40 +0000

    Refactor file status component dropdowns and buttons

--- app/frontend/src/components/FileStatus/FileStatus.tsx
+++ app/frontend/src/components/FileStatus/FileStatus.tsx
@@ -202,51 +202,60 @@ export const FileStatus = ({ className }: Props) => {
     return (
         <div className={styles.container}>
             <div className={`${styles.options} ${className ?? ""}`} >
-            <Dropdown
-                    label="Uploaded in last:"
-                    defaultSelectedKey='4hours'
-                    onChange={onTimeSpanChange}
-                    placeholder="Select a time range"
-                    options={dropdownTimespanOptions}
-                    styles={dropdownTimespanStyles}
-                    aria-label="timespan options for file statuses to be displayed"
+                <Dropdown
+                        label="Uploaded in last:"
+                        defaultSelectedKey='4hours'
+                        onChange={onTimeSpanChange}
+                        placeholder="Select a time range"
+                        options={dropdownTimespanOptions}
+                        styles={dropdownTimespanStyles}
+                        aria-label="timespan options for file statuses to be displayed"
+                    />
+                <Dropdown
+                        label="File State:"
+                        defaultSelectedKey={'ALL'}
+                        onChange={onFileStateChange}
+                        placeholder="Select file states"
+                        options={dropdownFileStateOptions}
+                        styles={dropdownFileStateStyles}
+                        aria-label="file state options for file statuses to be displayed"
+                    />
+                <Dropdown
+                    label="Folder:"
+                    defaultSelectedKey={'Root'}
+                    onChange={onFolderChange}
+                    placeholder="Select folder"
+                    options={folderOptions}
+                    styles={dropdownFolderStyles}
+                    aria-label="folder options for file statuses to be displayed"
                 />
-            <Dropdown
-                    label="File State:"
-                    defaultSelectedKey={'ALL'}
-                    onChange={onFileStateChange}
-                    placeholder="Select file states"
-                    options={dropdownFileStateOptions}
-                    styles={dropdownFileStateStyles}
-                    aria-label="file state options for file statuses to be displayed"
+                <Dropdown
+                    label="Tag:"
+                    defaultSelectedKey={'All'}
+                    onChange={onTagChange}
+                    placeholder="Select a tag"
+                    options={tagOptions}
+                    styles={dropdownTagStyles}
+                    aria-label="tag options for file statuses to be displayed"
                 />
-            <Dropdown
-                label="Folder:"
-                defaultSelectedKey={'Root'}
-                onChange={onFolderChange}
-                placeholder="Select folder"
-                options={folderOptions}
-                styles={dropdownFolderStyles}
-                aria-label="folder options for file statuses to be displayed"
-            />
-            <Dropdown
-                label="Tag:"
-                defaultSelectedKey={'All'}
-                onChange={onTagChange}
-                placeholder="Select a tag"
-                options={tagOptions}
-                styles={dropdownTagStyles}
-                aria-label="tag options for file statuses to be displayed"
-            />
-            <div className={styles.refresharea} onClick={onGetStatusClick} aria-label="Refresh displayed file statuses">
-                <ArrowClockwise24Filled className={styles.refreshicon} />
-                <span className={styles.refreshtext}>Refresh</span>
-            </div>
-
-
-
-
-            
+                <div className={styles.refresharea} onClick={onGetStatusClick} aria-label="Refresh displayed file statuses">
+                    <ArrowClockwise24Filled className={styles.refreshicon} />
+                    <span className={styles.refreshtext}>Refresh</span>
+                </div>
+                <div className={styles.buttonsContainer}>
+                    <div className={styles.refresharea} onClick={onGetStatusClick} aria-label="Refresh displayed file statuses">
+                        <ArrowClockwise24Filled className={styles.refreshicon} />
+                        <span className={styles.refreshtext}>Refresh</span>
+                    </div>
+                    <div className={`${styles.refresharea} ${styles.divSpacing}`} onClick={onDeleteClick} aria-label="Delete">
+                        <Delete24Regular className={styles.refreshicon} />
+                        <span className={`${styles.refreshtext} ${styles.centeredText}`}>Delete</span>
+                    </div>
+                    <div className={`${styles.refresharea} ${styles.divSpacing}`} onClick={onResubmitClick} aria-label="Resubmit">
+                        <Send24Regular className={styles.refreshicon} />
+                        <span className={`${styles.refreshtext} ${styles.centeredText}`}>Resubmit</span>
+                    </div>
+                </div>
             </div>
             {isLoading ? (
                 <animated.div style={{ ...animatedStyles }}>
