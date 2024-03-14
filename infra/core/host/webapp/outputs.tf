285e95900fb56dfd8a5bbe4450d9dea8a417d13f
George Earl <geearl@microsoft.com>
2024-03-14 22:23:33 +0000

    Add outputs for web app and service plan

--- infra/core/host/webapp/outputs.tf
+++ infra/core/host/webapp/outputs.tf
@@ -37,3 +37,16 @@ output "appServiceplanId" {
   description = "The app service plan id"
   value = azurerm_service_plan.appServicePlan.id
 }
+
+
+output "web_app_name" {
+  value = azurerm_linux_web_app.app_service.name
+}
+
+output "uri" {
+  value = "https://${azurerm_linux_web_app.app_service.default_hostname}"
+}
+
+output "web_serviceplan_name" {
+  value = azurerm_service_plan.appServicePlan.name
+}
